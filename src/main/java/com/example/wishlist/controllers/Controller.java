6fd64f18f610849341c659009aaf3f5813b61d2c
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2022-03-30 09:59:55 +0200

    added some html and css files. added dependencies from Nicklas's gitbook. auto-reload! added landingpage() in Controller.

--- src/main/java/com/example/wishlist/controllers/Controller.java
+++ src/main/java/com/example/wishlist/controllers/Controller.java
@@ -1,4 +1,11 @@
 package com.example.wishlist.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
 public class Controller {
+    @GetMapping("/")
+    public String landingpage() {
+        return "landingpage";
+    }
 }
