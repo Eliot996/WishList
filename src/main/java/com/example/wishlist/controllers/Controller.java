ec06ae9283f7c33f49c1bebf8da11fb86de497ea
Eliot996 <math10t6@stud.kea.dk>
2022-04-04 14:23:16 +0200

    working login

--- src/main/java/com/example/wishlist/controllers/Controller.java
+++ src/main/java/com/example/wishlist/controllers/Controller.java
@@ -1,6 +1,5 @@
 package com.example.wishlist.controllers;
 
-import com.example.wishlist.models.Wishlist;
 import com.example.wishlist.models.User;
 import com.example.wishlist.repository.DummyWishlistRepo;
 import com.example.wishlist.services.UserService;
@@ -8,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -19,8 +20,23 @@ public class Controller {
     }
 
     @GetMapping("/login")
-    public String loginPage(){
-        return "login";
+    public String loginPage(HttpSession session, Model model){
+        if (session.getAttribute("user_token") == null){
+            model.addAttribute("user", new User());
+            return "login";
+        }
+        return "redirect:/lists";
+    }
+
+    @PostMapping("/login")
+    public String loginPage(HttpSession session, @ModelAttribute User user, Model model){
+        String token = USER_SERVICE.login(user.getEmail(), user.getPassword());
+
+        if (token != null) {
+            session.setAttribute("user_token", token);
+        }
+
+        return "registrer";
     }
 
     @GetMapping("/registrer")
