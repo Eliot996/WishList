8b70c89ecf1d3fb942fee3679faea3165918f14e
Eliot996 <math10t6@stud.kea.dk>
2022-04-04 12:49:44 +0200

    added post getting for adding users

--- src/main/java/com/example/wishlist/controllers/Controller.java
+++ src/main/java/com/example/wishlist/controllers/Controller.java
@@ -1,11 +1,16 @@
 package com.example.wishlist.controllers;
 
+import com.example.wishlist.models.User;
 import com.example.wishlist.repository.DummyWishlistRepo;
+import com.example.wishlist.services.UserService;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+    private final UserService USER_SERVICE = new UserService();
 
     @GetMapping("/")
     public String landing() {
@@ -18,8 +23,15 @@ public class Controller {
     }
 
     @GetMapping("/registrer")
-    public String registrerPage(){
+    public String registrerPage(Model model){
+        model.addAttribute("user", new User());
         return "registrer";
+    }
+
+    @PostMapping("/registrer")
+    public String registrerSubmit(@ModelAttribute User user, Model model){
+        USER_SERVICE.createUser(user.getName(), user.getEmail(), user.getPassword());
+        return "landingpage";
     }
 
     @GetMapping("/lists")
