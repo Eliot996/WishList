f314d7cfd2b5f9b5d2da0dc735140acab46ccb4b
Eliot996 <math10t6@stud.kea.dk>
2022-04-04 10:33:33 +0200

    added ConnectionManager, with simple functionality

---
+++ src/main/java/com/example/wishlist/repository/ConnectionManager.java
@@ -0,0 +1,35 @@
+package com.example.wishlist.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionManager {
+    private static final ConnectionManager INSTANCE = new ConnectionManager();
+    private Connection connection;
+
+    private ConnectionManager() {
+        connectDB();
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void connectDB()
+    {
+        try
+        {
+            String url = "jdbc:mysql://wishlist-kea-database.mysql.database.azure.com:3306/wishlist";
+            connection = DriverManager.getConnection(url,"Main_User@wishlist-kea-database","$UperPassword99");
+            System.out.println("Ok, we have a connection.");
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static ConnectionManager getInstance() {
+        return INSTANCE;
+    }
+}
