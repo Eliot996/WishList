d1a0d72f2da36d5bb3df91c7e1ed223492f3efe6
Eliot996 <math10t6@stud.kea.dk>
2022-04-04 11:06:21 +0200

    added createUser

--- src/main/java/com/example/wishlist/repository/UserRepo.java
+++ src/main/java/com/example/wishlist/repository/UserRepo.java
@@ -1,4 +1,51 @@
 package com.example.wishlist.repository;
 
+import com.example.wishlist.models.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserRepo {
+
+    private static final UserRepo INSTANCE = new UserRepo();
+    private ConnectionManager connectionManager = ConnectionManager.getInstance();
+
+    private UserRepo() {
+
+    }
+
+    public User createUser(String name, String email, String password, String salt) {
+        Connection con = connectionManager.getConnection();
+
+        String insertSQL = "INSERT INTO users(name, email, password, salt)" +
+                           "VALUES (?, ?, ?, ?)";
+
+        PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement(insertSQL);
+            stmt.setString(1, name);
+            stmt.setString(2, email);
+            stmt.setString(3, password);
+            stmt.setString(4, salt);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            stmt.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void getUser() {
+
+    }
+
+    public static UserRepo getInstance() {
+        return INSTANCE;
+    }
 }
