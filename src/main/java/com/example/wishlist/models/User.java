f2e7a5c7b861d56cb6d88f8f64813c837a914192
Eliot996 <math10t6@stud.kea.dk>
2022-04-04 10:49:03 +0200

    added salt to user

--- src/main/java/com/example/wishlist/models/User.java
+++ src/main/java/com/example/wishlist/models/User.java
@@ -5,11 +5,13 @@ public class User {
     private String name;
     private String email;
     private String password;
+    private String salt;
 
-    public User(int ID, String name, String email, String password) {
+    public User(int ID, String name, String email, String password, String salt) {
         this.ID = ID;
         this.name = name;
         this.email = email;
         this.password = password;
+        this.salt = salt;
     }
 }
