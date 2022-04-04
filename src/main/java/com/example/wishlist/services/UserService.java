f426f044f542094813243b25313a5f43e8072623
Eliot996 <math10t6@stud.kea.dk>
2022-04-04 11:29:12 +0200

    added get by userid

--- src/main/java/com/example/wishlist/services/UserService.java
+++ src/main/java/com/example/wishlist/services/UserService.java
@@ -1,4 +1,12 @@
 package com.example.wishlist.services;
 
+import com.example.wishlist.models.User;
+import com.example.wishlist.repository.UserRepo;
+
 public class UserService {
+    private final UserRepo USER_REPO = UserRepo.getInstance();
+
+    public User getUser(int id) {
+        return USER_REPO.getUser(id);
+    }
 }
