b6d95a676063230606db4b3a89672976343199ee
Eliot996 <math10t6@stud.kea.dk>
2022-04-05 15:01:41 +0200

    added populateWishlist()

--- src/main/java/com/example/wishlist/services/WishService.java
+++ src/main/java/com/example/wishlist/services/WishService.java
@@ -1,4 +1,19 @@
 package com.example.wishlist.services;
 
+import com.example.wishlist.models.Wish;
+import com.example.wishlist.models.Wishlist;
+import com.example.wishlist.repository.WishRepo;
+
+import java.util.List;
+
 public class WishService {
+    private WishRepo WISH_REPO = WishRepo.getINSTANCE();
+
+    public void populateWishlist(Wishlist wishlist) {
+        // get wishes from database
+        List<Wish> listOfWishes = WISH_REPO.getWishes(wishlist.getID());
+
+        // bind to wishlist
+        wishlist.setWishes(listOfWishes);
+    }
 }
