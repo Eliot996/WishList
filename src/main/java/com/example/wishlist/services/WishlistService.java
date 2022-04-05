07d0b96f72242a909de2b861d6dd413ed91c4618
Eliot996 <math10t6@stud.kea.dk>
2022-04-05 12:15:12 +0200

    added create wishlist

--- src/main/java/com/example/wishlist/services/WishlistService.java
+++ src/main/java/com/example/wishlist/services/WishlistService.java
@@ -1,4 +1,12 @@
 package com.example.wishlist.services;
 
+import com.example.wishlist.models.Wishlist;
+import com.example.wishlist.repository.WishlistRepo;
+
 public class WishlistService {
+    private final WishlistRepo WISHLIST_REPO = WishlistRepo.getINSTANCE();
+
+    public int createWishlist(int userID, Wishlist wishlist) {
+        return WISHLIST_REPO.createWishlist(userID, wishlist);
+    }
 }
