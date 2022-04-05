214f297ed29b802eba4cfd11d69bfcdbf76993f1
Eliot996 <math10t6@stud.kea.dk>
2022-04-05 14:34:18 +0200

    updated and added setters and getters

--- src/main/java/com/example/wishlist/models/Wish.java
+++ src/main/java/com/example/wishlist/models/Wish.java
@@ -1,14 +1,19 @@
 package com.example.wishlist.models;
 
 public class Wish {
-    private Wishlist wishlistID;
+    private int wishlistID;
     private int position;
     private String title;
     private String description;
     private String link;
-    private User reserver;
+    private int reserver;
 
-    public Wish(Wishlist wishlistID, int position, String title, String description, String link, User reserver) {
+
+    public Wish(){
+    }
+
+
+    public Wish(int wishlistID, int position, String title, String description, String link, int reserver) {
         this.wishlistID = wishlistID;
         this.position = position;
         this.title = title;
@@ -17,13 +22,51 @@ public class Wish {
         this.reserver = reserver;
     }
 
-    public Wish(){
-        this.wishlistID = null;
-        this.position = 0;
-        this.title = null;
-        this.description = null;
-        this.link = null;
-        this.reserver = null;
+    public int getWishlistID() {
+        return wishlistID;
     }
 
+    public void setWishlistID(int wishlistID) {
+        this.wishlistID = wishlistID;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public int getReserver() {
+        return reserver;
+    }
+
+    public void setReserver(int reserver) {
+        this.reserver = reserver;
+    }
 }
